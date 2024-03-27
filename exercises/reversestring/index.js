b494ac7a8aecc0ceb26a0916672830600733dea2
valon <valonloshaj@gmail.com>
2024-03-27 17:02:12 -0400

    Add reverse string function implementation

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+	// Step 1: Turn str into an array using .split
+	// Step 2: Use reduce to
+	return str
+		.split("")
+		.reduce((reversed, character) => character + reversed, "");
+}
 
 module.exports = reverse;
+
+// Solution #1:
+// Step 1: Turn the string into an array using .split
+// const arr = str.split("");
+// Step 2: Reverse the array using .reverse
+// arr.reverse();
+// Step 3: Convert that reversed array into a string
+// return arr.join("");
+
+// Solution #2:
+// Step 1: Creat an empty string
+// let reversed = "";
+// Step 2: Loop through str and add the characters to the new empty string
+// for (let character of str) {
+// 	reversed = character + reversed;
+// }
+// Step 3: Return the new string
+// return reversed;
