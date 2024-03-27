65c6a2541387f5572ef3049885fac05c35ecf8c1
valon <valonloshaj@gmail.com>
2024-03-27 18:47:08 -0400

    Add fizzBuzz function implementation

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,23 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+	// Solution 1:
+	// Step 1: Check if n is a number
+	// Step 2: Check if a number is a multiple of 3
+	// Step 3: Check if a number is a multiple of 5
+	// Step 4: Check if a number is a multiple of 3 & 5
+	for (let i = 1; i <= n; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			console.log("fizzbuzz");
+		} else if (i % 3 === 0) {
+			console.log("fizz");
+		} else if (i % 5 === 0) {
+			console.log("buzz");
+		} else {
+			console.log(i);
+		}
+	}
+}
 
 module.exports = fizzBuzz;
