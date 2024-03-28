d0728fc7619fe26be6c491dee6bd0eee3b46a078
valon <valonloshaj@gmail.com>
2024-03-28 18:48:41 -0400

    Refactor anagrams function and add test cases

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,48 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+	// Solution 1 (didn't work)
+	// Step 1: Use RegExp to remove spaces and punctuations const word = "HI THERE!!" word.replace(/[^/w]/g)
+	// Step 2: Use .toLowerCase() to make both strings lower case
+	// Step 3: Check if both strings are of equal length, if not, return false
+	// Step 4: Turn each string into an array using split
+	// Step 5: Sort the arrays
+	// Step 6: Loop over the first array and check if it is equal to the second array
+	return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+	return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
 
 module.exports = anagrams;
+
+// Solution 2
+// Step 1: Create a charMap for each cleansed string (use helper function)
+// Step 2: Compare the length of the map keys to ensure they are the same length
+// Step 3: Loop through one of the char maps and compare each value to the other charMap
+// 	const aCharMap = buildCharMap(stringA);
+// 	const bCharMap = buildCharMap(stringB);
+
+// 	if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+// 		return false;
+// 	}
+
+// 	for (let char in aCharMap) {
+// 		if (aCharMap[char] !== bCharMap[char]) {
+// 			return false;
+// 		}
+// 	}
+
+// 	return true;
+// }
+
+// function buildCharMap(str) {
+// 	const charMap = {};
+
+// 	for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+// 		charMap[char] = charMap[char] + 1 || 1;
+// 	}
+
+// 	return charMap;
