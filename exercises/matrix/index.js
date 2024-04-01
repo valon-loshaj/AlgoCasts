6f62129807fe00d646b13f00a5ddf67d3d392b18
valon <valonloshaj@gmail.com>
2024-04-01 18:46:17 -0400

    Refactor matrix and vowels functions

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,65 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+/*
+Solution 1
+Create empty array of arrays called 'results'
+Create a count variable, starting at 1
+As long as (start column <= end column) AND (start row <= end row)
+	Loop from start column to end column
+		At results[start_row][i] assign counter variable
+		Increment counter
+	Increment start row
+	Loop from start row to end row
+		At results[i][end_column] assign counter variable
+		Increment counter
+	Decrement end row
+	...repeat for other two sides
+*/
+
+function matrix(n) {
+	const results = [];
+
+	for (let i = 0; i < n; i++) {
+		results.push([]);
+	}
+
+	let counter = 1;
+	let startColumn = 0;
+	let endColumn = n - 1;
+	let startRow = 0;
+	let endRow = n - 1;
+	while (startColumn <= endColumn && startRow <= endRow) {
+		// Top row
+		for (let i = startColumn; i <= endColumn; i++) {
+			results[startRow][i] = counter;
+			counter++;
+		}
+		startRow++;
+
+		// Right column
+		for (let i = startRow; i <= endRow; i++) {
+			results[i][endColumn] = counter;
+			counter++;
+		}
+		endColumn--;
+
+		// Bottom row
+		for (let i = endColumn; i >= startColumn; i--) {
+			results[endRow][i] = counter;
+			counter++;
+		}
+		endRow--;
+
+		// start column
+		for (let i = endRow; i >= startRow; i--) {
+			results[i][startColumn] = counter;
+			counter++;
+		}
+		startColumn++;
+	}
+
+	return results;
+}
 
 module.exports = matrix;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+	const matches = str.match(/[aeiou]/gi);
+	return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+/*
+Solution 2
+let vowelCount = 0;
+const checker = ["a", "e", "i", "o", "u"];
+for (let char of str.toLowerCase()) {
+	if (checker.includes(char)) {
+		vowelCount++;
+	}
+}
+return vowelCount;
+*/
